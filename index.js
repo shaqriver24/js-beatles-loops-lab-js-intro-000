5e363b546e30bbbe51de3a9616d83ebe76d408a3
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2019-11-18 07:45:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // add solution here
+  function theBeatlesPlay(musicians, instruments) {
+    let beatles = []
+    for (let i = 0; i < musicians.length; i++) {
+      let tbp = `${musicians[i]} plays ${instruments[i]}`
+      beatles.push(tbp)
+    }
+    return beatles
+  }
+
+function johnLennonFacts(facts) {
+  let i = 0
+  while (i < facts.length) {
+    facts[i] = facts[i] + "!!!"
+    i++
+  }
+  return facts
+}
+
+function iLoveTheBeatles(num) {
+  var empty = []
+  do {
+    empty.push("I love the Beatles!")
+    num++
+  }while(num < 15)
+  return empty
+}
